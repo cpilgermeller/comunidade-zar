365fccb12562ae5260a54f22bdbd5357b7df8472
Camila Meller <cpilgermeller@gmail.com>
2026-05-14 11:17:49 -0300

    fix: remove adapter from prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -8,9 +7,4 @@ export default defineConfig({
     path: "prisma/migrations",
     seed: "npx tsx prisma/seed.ts",
   },
-  adapter: () =>
-    new PrismaLibSql({
-      url: process.env.DATABASE_URL!,
-      authToken: process.env.TURSO_AUTH_TOKEN,
-    }),
 });
